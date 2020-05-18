e1bd1b282e33bc7dfde6247cdbcbfacd66074f21
snigo <snigo.ogins@gmail.com>
2020-05-18 22:13:45 +0200

    Add next-offline package to work with workbox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,36 @@
+import Head from 'next/head';
+
 import '../styles/main.scss';
 
-const Snigo = ({ Component, pageProps }) => <Component {...pageProps} />;
+const Snigo = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="application-name" content="@snigo" />
+      <meta name="apple-mobile-web-app-title" content="@snigo" />
+      <meta name="theme-color" content="#fafeff" />
+      <meta name="msapplication-navbutton-color" content="#fafeff" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="msapplication-starturl" content="/" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" type="image/svg+xml" href="/images/ninja-icon.svg" />
+      <link rel="alternate icon" href="/images/ninja-icon-32.png" />
+      <link rel="icon" sizes="128x128" href="/images/ninja-icon-128.png" />
+      <link rel="apple-touch-icon" sizes="128x128" href="/images/ninja-icon-128.png" />
+      <link rel="icon" sizes="192x192" href="/images/ninja-icon-192.png" />
+      <link rel="apple-touch-icon" sizes="192x192" href="/images/ninja-icon-192.png" />
+      <link rel="icon" sizes="256x256" href="/images/ninja-icon-256.png" />
+      <link rel="apple-touch-icon" sizes="256x256" href="/images/ninja-icon-256.png" />
+      <link rel="icon" sizes="384x384" href="/images/ninja-icon-384.png" />
+      <link rel="apple-touch-icon" sizes="384x384" href="/images/ninja-icon-384.png" />
+      <link rel="icon" sizes="512x512" href="/images/ninja-icon-512.png" />
+      <link rel="apple-touch-icon" sizes="512x512" href="/images/ninja-icon-512.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default Snigo;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,29 +10,26 @@ function getChallengeDay() {
   return Math.ceil((Date.now() - START_DATE.getTime()) / (3600000 * 24));
 }
 
-const Index = () => (
-  <div className="shell">
-    <Head>
-      <meta charSet="utf-8" />
-      <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
-      <title>@snigo: with love to web.</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" type="image/svg+xml" href="/images/ninja-icon.svg" />
-      <link rel="alternate icon" href="/images/ninja-icon-32.png" />
-      <meta name="description" content="@snigo is an aspiring web developer with passion to open source, modern web standards and UX-driven software design."/>
-      <meta name="keywords" content="graphic design, web design, website design, website builder, web developer, web designer, webdesign, ecommerce website, web design company, website creator, website designer, responsive web design, web development company, best website design, web design software, web page design, build a website, design website, web design inspiration, website layout, react website, react developer" />
-    </Head>
-    <div className="wrapper">
-      <h1 className="title">Hi, I'm @snigo<span style={{color: 'var(--colors-blue-50'}}>.</span></h1>
-      <p>I've accepted <a href="https://www.100daysofcode.com/" target="_blank" rel="noreferrer noopener" className="fancy-link">#100DaysOfCode</a> challenge and this is my day <span style={{color: 'var(--colors-pink-40'}}>{getChallengeDay()}</span>.</p>
-      <hr/>
-      <div className="project-grid">
-        {
-          projects.reverse().map((task) => <TaskCard task={task as DayTask} key={task.date} />)
-        }
+const Index = () => {
+  return (
+    <div className="shell">
+      <Head>
+        <title>@snigo: with love to web.</title>
+        <meta name="description" content="@snigo is an aspiring web developer with passion to open source, modern web standards and UX-driven software design."/>
+        <meta name="keywords" content="graphic design, web design, website design, website builder, web developer, web designer, webdesign, ecommerce website, web design company, website creator, website designer, responsive web design, web development company, best website design, web design software, web page design, build a website, design website, web design inspiration, website layout, react website, react developer" />
+      </Head>
+      <div className="wrapper">
+        <h1 className="title">Hi, I'm @snigo<span style={{color: 'var(--colors-blue-50'}}>.</span></h1>
+        <p>I've accepted <a href="https://www.100daysofcode.com/" target="_blank" rel="noreferrer noopener" className="fancy-link">#100DaysOfCode</a> challenge and this is my day <span style={{color: 'var(--colors-pink-40'}}>{getChallengeDay()}</span>.</p>
+        <hr/>
+        <div className="project-grid">
+          {
+            projects.reverse().map((task) => <TaskCard task={task as DayTask} key={task.date} />)
+          }
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Index;
